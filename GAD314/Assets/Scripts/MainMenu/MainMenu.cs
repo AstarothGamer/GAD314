2b6fb06fbb4d8e8118a3e88e9567cdb21dce9969
Efim Egorov <efim.egorov@student.saedubai.com>
2025-10-23 13:26:59 +0400

    Changes into the main menu

--- GAD314/Assets/Scripts/MainMenu/MainMenu.cs
+++ GAD314/Assets/Scripts/MainMenu/MainMenu.cs
@@ -3,19 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void ShootScene()
-    {
-        SceneManager.LoadScene(2);
-    }
-
-    public void GrapplingScene()
+    public void NewGame()
     {
         SceneManager.LoadScene(1);
-    }
-
-    public void TimeStopScene()
-    {
-        SceneManager.LoadScene(3);
     }
     
     public void Exit()
