d6a8a45a30553e142e60b2bc04237339f696eef0
Efim Egorov <efim.egorov@student.saedubai.com>
2025-10-29 13:09:07 +0400

    reupload playermovement2 script and added basic weaponmanager script

---
+++ GAD314/Assets/Scripts/Character/WeaponManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WeaponManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainGun;
+    [SerializeField] private GameObject sword;
+    [SerializeField] private GameObject grenadeLouncher;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WeaponSwitch();
+    }
+    
+    private void WeaponSwitch()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            mainGun.SetActive(true);
+            sword.SetActive(false);
+            grenadeLouncher.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            mainGun.SetActive(false);
+            sword.SetActive(true);
+            grenadeLouncher.SetActive(false);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            mainGun.SetActive(false);
+            sword.SetActive(false);
+            grenadeLouncher.SetActive(true);
+        }
+    }
+}
