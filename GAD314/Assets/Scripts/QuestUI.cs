84e5f51ef7e00f616f3cad9eebde8420f127d6b8
Jeffrey L. Salvino II <jeffrey.salvino@student.saedubai.com>
2025-12-04 14:00:16 +0400

    Pushing working build

---
+++ GAD314/Assets/Scripts/QuestUI.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+public class QuestUI : MonoBehaviour
+{
+    //put the quest text from the canvas here
+    public TMP_Text questText;
+
+    // references to the player's weapons
+    public GameObject katanaObject;
+    public GameObject gunObject;
+
+
+    void Update()
+    {
+        // If player has no katana active, the quest is to find it
+        if (!katanaObject.activeSelf)
+        {
+            questText.text = "Quest: Find a Katana";
+        }
+        // If katana is active but gun is not, next quest
+        else if (katanaObject.activeSelf && !gunObject.activeSelf)
+        {
+            questText.text = "Quest: Find a Gun";
+        }
+        // If both are active, quest is done
+        else if (katanaObject.activeSelf && gunObject.activeSelf)
+        {
+            questText.text = "Quest: All weapons found!";
+        }
+    }
+}
